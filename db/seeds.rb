7bb191e6d860827d222e6a63ceb11f426fb7507f
cedricchaynes <cedric.chaynes@outlook.com>
2019-05-17 11:39:17 +0200

    UNFINISHED Mister Cocktail exercice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create!(name: "lemon")
+Ingredient.create!(name: "ice")
+Ingredient.create!(name: "mint leaves")
+
+Cocktail.create!(name: "lemon cocktails")
+Cocktail.create!(name: "ice cocktails")
+Cocktail.create!(name: "mint cocktails")
+
+# Doses.create(name: "1")
+# Doses.create(name: "2")
+# Doses.create(name: "3")
